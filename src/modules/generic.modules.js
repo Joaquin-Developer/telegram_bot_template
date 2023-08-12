95c656417eca6499dde0f9cc8e6c0a7bb7db564c
Joaquin-Parrilla <jparrilla@scanntech.com>
2023-08-12 15:56:07 -0300

    import config en generic.modules

--- src/modules/generic.modules.js
+++ src/modules/generic.modules.js
@@ -1,5 +1,6 @@
 const dbController = require("./db")
 const { MongoDBInsertError } = require("../exceptions/exceptions")
+const config = require("../../config")
 const utils = require("../utils/utils")
 
 const functions = {}
